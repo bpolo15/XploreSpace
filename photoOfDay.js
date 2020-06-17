85b069e12953f50d631e3980aa9c52a80a36718e
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-06-17 16:14:54 -0500

    fixed video/photo bug

--- photoOfDay.js
+++ photoOfDay.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
     var mm = parseInt(String(date.getMonth() + 1).padStart(2, '0')); //January is 0!
     var yyyy = date.getFullYear();
     photoOfDay(yyyy,mm,dd);
-    
+   
 
 function photoOfDay(yyyy,mm,dd){
-    
+ 
+
+
     $("#todayDate").text(mm+"-"+dd+"-"+yyyy)
     
     var api_key = "tEZcdcYgqv4qRNe0W8QrLu2ed5kywhjxxLWvofzI"
@@ -21,15 +23,19 @@ function photoOfDay(yyyy,mm,dd){
         var photo = response.url;
         var title = response.title;
         console.log(title)
+        
         if(photo.includes("youtube")){
             console.log("youtube")
-            // $('<iframe src="' +photo + '" width=560 height=315 frameborder="0" scrolling="no" id="iframe" </iframe>').appendTo("#photoHere")
+            $("#photoSpot").hide("#photoHere")
+            $('.video').show("#videoHere")
             $("#iframe").attr("src", photo)
             $("#photoTitle").text(title)
+
         } else{
+            $("#photoSpot").show("#photoHere")
             $("#photoTitle").text(title)
             $("#photoHere").css("background-image", 'url("' + photo +'")')
-            $("div").remove(".video")
+            $('.video').hide("#videoHere")
           
             
         }
@@ -59,11 +65,17 @@ function photoOfDay(yyyy,mm,dd){
             mm = 12
         }
         console.log(dd)
-        photoOfDay(yyyy,mm,dd)
+        // photoOfDay(yyyy,mm,dd)
+        removeCard(yyyy,mm,dd)
         
         
 
     })
+    function removeCard(yyyy,mm,dd){
+        $('div').remove(".photoOfDay")
+        photoOfDay(yyyy,mm,dd)
+    }
+
     // $("#rightbtn").click(function(){
     //     var current = parseInt(String(date.getDate()).padStart(2, '0'));
     //     console.log("right")
@@ -86,7 +98,7 @@ function photoOfDay(yyyy,mm,dd){
     // })
     
 
- 
+ //  $('<iframe src="' +photo + '" width=560 height=315 frameborder="0" scrolling="no" id="iframe" </iframe>').appendTo("#photoHere")
 
 
 })
